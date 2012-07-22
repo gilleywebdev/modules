c023a499aa7657be456b6249de688db6a2d096c7
Chris Gilley <chris@gilleywebdev.com>
2012-07-21 20:06:57 -0400

    Revert "Revert "deleted admin""
    This reverts commit 3d1cbc06d552cdeaad3a2b25865f428ab4151a1a.

--- admin/views/admin/includes/navigation.php
+++
@@ -1,5 +0,0 @@
-<h1><a href="/admin/" title="Site Name">RBM</a></h1>
-<ul>
-	<li id="news_nav"><a href="/admin/news" title="News">News</a></li>
-	<li id="news_nav"><a href="/admin/seo" title="SEO">SEO</a></li>
-</ul>
--- admin/views/admin/seo.php
+++
@@ -1,7 +0,0 @@
-<p>Hello SEO world</p>
-<?php
-	foreach($pages AS $page)
-	{
-		echo '<p>'.$page.'</p>';
-	}
-?>
