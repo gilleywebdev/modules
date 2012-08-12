07fb10e646b9643e263e0a1915b1ea8225bb13ba
Chris Gilley <chris@gilleywebdev.com>
2012-08-11 21:59:17 -0400

    moved a file

--- admin/messages/admin/user.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array(
-	'deleted' => 'User successfully deleted',
-	'edited' => 'User successfully edited',
-	'added' => 'User successfully added',
-);
