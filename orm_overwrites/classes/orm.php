b67deffe8f886e60d99ab94bf6f919d7540275bc
Chris Gilley <chris@gilleywebdev.com>
2012-08-19 00:51:29 -0400

    Made the new ORM::create play nice

--- orm_overwrites/classes/orm.php
+++ orm_overwrites/classes/orm.php
@@ -2,12 +2,35 @@
 
 class ORM extends Kohana_ORM {
 	// Simplified "create" wrapper
-	public function create(Validation $validation = NULL, array $values = array())
+	public function create(Validation $validation = NULL)
 	{
-		return $this->values($validation->data(), $values)->parent_create($validation);
+		// Can be called with array as second parameter in controller
+		if (func_num_args() >= 2)
+		{
+			$values = func_get_arg(1);
+			if ( ! is_array($values))
+			{
+				throw new Kohana_Exception('second argument must be array');
+			}
+		}
+		else
+		{
+			$values = NULL;
+		}
+
+		if ($values !== NULL)
+		{
+			// New function
+			return $this->values($validation->data(), $values)->parent_create($validation);
+		}
+		else
+		{
+			// Default function
+			return parent::create($validation);
+		}
 	}
 	
-	private function parent_create(Validation $validation)
+	private parent_create(Validation $validation = NULL)
 	{
 		return parent::create($validation);
 	}
