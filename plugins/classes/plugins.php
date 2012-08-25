fa5c8dd5cfb70cb5068de6390d38609ba884dbe0
Chris Gilley <chris@gilleywebdev.com>
2012-08-24 23:57:47 -0400

    Fixed plugins error for plugins with no CSS

--- plugins/classes/plugins.php
+++ plugins/classes/plugins.php
@@ -6,23 +6,25 @@ class Plugins{
 		$plugins = Kohana::$config->load('plugins');
 		if (! empty($plugins['enabled'][$profile]))
 		{
-			foreach($plugins['enabled'][$profile] AS $plugin)
+			foreach ($plugins['enabled'][$profile] AS $plugin)
 			{
 				if ($plugin = $plugins['available'][$plugin])
 				{					
 					// Styles
-					if ($styles = $plugin['styles'])
+					if (isset($plugin['styles']))
 					{
-						foreach($styles AS $style)
+						$styles = $plugin['styles'];
+						foreach ($styles AS $style)
 						{
 							Styles::add($style[0], $style[1]);
 						}
 					}
 
 					// Scripts
-					if ($scripts = $plugin['scripts'])
-					{						
-						foreach($scripts AS $script)
+					if (isset($plugin['scripts']))
+					{
+						$scripts = $plugin['scripts'];
+						foreach ($scripts AS $script)
 						{
 							Scripts::add($script[0], $script[1]);
 						}
