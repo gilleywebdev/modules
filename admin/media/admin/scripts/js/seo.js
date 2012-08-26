ff2ccaf62179c7452220c1ab6d334a0bfe08dda8
Chris Gilley <chris@gilleywebdev.com>
2012-08-26 01:09:19 -0400

    Google SERP preview

--- admin/media/admin/scripts/js/seo.js
+++ admin/media/admin/scripts/js/seo.js
@@ -1,4 +1,5 @@
 var container = $(".spreadsheet");
+var $google = $('.google');
 
 container.handsontable({
     rows: 3,
@@ -13,12 +14,20 @@ container.handsontable({
 		}
 	],
     rowHeaders: false,
-    colHeaders: ["Page", "Title", "Description"]
-
+    colHeaders: ["Page", "Title", "Description"],
+	onChange: function (change) {
+		var width = $('.spreadsheet table').width();
+		
+		var googlePos = width + 15;
+		
+		$google.css('left', googlePos);
+		console.log(currentPos);
+	}
 });
 
 var handsontable = container.data('handsontable');
 
+// Load data from database on pageload
 $.ajax({
 	url: "/admin/seo/load",
 	dataType: 'json',
@@ -28,6 +37,7 @@ $.ajax({
 	}
 });
 
+// Save to database
 $('.submit').click(function () {
 	$.ajax({
 		url: "/admin/seo/save",
@@ -38,4 +48,65 @@ $('.submit').click(function () {
 			window.location.href = "/admin/seo/index/success/seo_updated";
 		}
 	});
+});
+
+// Google Preview
+
+
+container.mousedown(function () {
+	// Move google to selected positon
+	var currentPos = $('.current').position();
+	$google.css('top', currentPos.top - 15);
+	$google.hide();
+});
+
+container.mouseup(function () {
+	selected = handsontable.getSelected(); // returns [`topLeftRow`, `topLeftCol`, `bottomRightRow`, `bottomRightCol`]
+
+	if(selected[0] === selected[2]) { // if a single row is selected
+		// Show google
+		$google.show();
+
+		// Get the data
+		x = selected[0];
+		y = selected[1];
+		var pagename = handsontable.getDataAtCell(x, 0);
+		var title = handsontable.getDataAtCell(x, 1)
+		var description = handsontable.getDataAtCell(x, 2);
+		
+		// Truncate if necessary
+		if(title.length > 69)
+		{
+			title = jQuery.trim(title).substring(0, 69)
+			    .split(" ").slice(0, -1).join(" ") + "...";
+		}
+		
+		if(description.length > 156)
+		{
+			description = jQuery.trim(description).substring(0, 156)
+			    .split(" ").slice(0, -1).join(" ") + "...";
+		}
+
+		// Format the data
+		var formatted = '<p class="google_title">' + title + '</p>';
+		formatted = formatted + '<p class="google_link">http://www.gilleywebdev.com/' + pagename + '</p>';
+		formatted = formatted + '<p class="google_description">' + description + '</p>';
+
+		// Put the data in the div
+		$google.html(formatted);
+		
+		$(this).data('y', y);
+	}
+});
+
+$('.handsontableInput').keyup(function () {
+	// Title
+	if (container.data('y') === 1) {
+		$('.google_title').text($(this).val());
+	}
+	
+	// Description
+	if (container.data('y') === 2) {
+		$('.google_description').text($(this).val());		
+	}
 });
--- admin/views/admin/seo/index.php
+++ admin/views/admin/seo/index.php
@@ -1,3 +1,5 @@
 <?php echo View::factory('includes/forms/errors') ?>
-<div class="spreadsheet"></div>
+<div class="spreadsheet">
+	<div class="google"></div>
+</div>
 <?php echo Form::submit(NULL, 'Save') ?>
