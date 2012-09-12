8e05a9361b50a25066834f8a5501edc531261ebd
Chris Gilley <chris@gilleywebdev.com>
2012-09-12 16:11:09 -0400

    Improved Info::address view and moved it from admin_info to info module

--- admin_info/views/includes/address.php
+++
@@ -1,4 +0,0 @@
-<p><?php echo $address_1; ?><br>
-<?php echo $address_2; ?><br>
-<?php echo $city; ?><br>
-<?php echo $zip; ?></p>
---
+++ info/views/includes/address.php
@@ -0,0 +1,6 @@
+<p><?php echo $address_1 ?><br>
+<?php if ($address_2): ?>
+	<?php echo $address_2 ?><br>
+<?php endif ?>
+<?php echo $city ?><br>
+<?php echo $zip ?></p>
