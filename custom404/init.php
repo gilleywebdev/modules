f4876a42a67c4ae25465cae57f4267ee2217b9f2
Chris Gilley <chris@gilleywebdev.com>
2012-10-29 15:08:54 -0400

    Removed broken exception handler in 404 module

--- custom404/init.php
+++ custom404/init.php
@@ -4,8 +4,3 @@ Route::set('error', 'error/<action>(/<message>)', array('action' => '[0-9]++', '
 ->defaults(array(
     'controller' => 'Error_Handler',
 ));
-
-if (Kohana::$errors === FALSE)
-{
-	set_exception_handler(array('Kohana_Exception_Custom', 'handler'));
-}
