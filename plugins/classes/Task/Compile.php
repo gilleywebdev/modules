09d2f679fc95ba29dfa7efba90b4e9bb9ee16355
Chris Gilley <chris@gilleywebdev.com>
2013-01-26 21:59:34 -0500

    more work on plugins compile script

--- plugins/classes/Task/Compile.php
+++ plugins/classes/Task/Compile.php
@@ -5,23 +5,22 @@ class Task_Compile extends Minion_Task
 {
 	protected function _execute(array $params)
     {
-		$list_all = Kohana::list_files('media/styles/css');
-	
-		$profile = 'default';
+		// Styles
+		$styles_config = Kohana::$config->load('styles');
 
-		$files = Styles::prepare_production_file($profile);
-
-		foreach($files AS $file)
-		{
-			$path = $file['path'];
-			$ext = $file['ext'];
-		}
+		$styles = array();
 		
+		foreach ($styles_config as $profile => $files)
+		{
+			$files = Styles::prepare_production_file($profile);
+
+			$styles[$profile] = $files;
+		}
+
 		$view = new View('minion/compile');
 
-		$view->files = $files;
-		$view->list = $list_all;
+		$view->styles = $styles;
 
 		echo $view;
-    }
+	}
 }
--- plugins/views/minion/compile.php
+++ plugins/views/minion/compile.php
@@ -1,10 +1,9 @@
-Production Files:
+<?php foreach ($styles as $profile => $files): ?>
+Compiling styles for profile <?php echo $profile ?>:
 
 <?php foreach ($files as $file): ?>
 	<?php echo $file['path'].'.'.$file['ext'] ?>
 
 <?php endforeach ?>
 
-List all: 
-
-<?php var_dump($list) ?>
+<?php endforeach ?>
