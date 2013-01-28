0b829423e9fd6114b1315b4eb88a369e12f65dc3
Chris Gilley <chris@gilleywebdev.com>
2013-01-28 17:31:54 -0500

    Band-aid fix of admin Plugins::add calls before rewrite

--- admin/classes/controller/admin/auth.php
+++ admin/classes/controller/admin/auth.php
@@ -31,8 +31,8 @@ class Controller_Admin_Auth extends Controller_Template {
 		}
 
 		// View
-		Styles::add('admin/login', Styles::PAGE);
-		Scripts::add('admin/login', Scripts::CONTROLLER);
+		Styles::add('admin/login', Styles::PAGE, 'styles', 'admin');
+		Scripts::add('admin/login', Scripts::CONTROLLER, 'scripts', 'admin_outside');
 		$this->template->content = View::factory('admin/auth/login');
 		$this->template->title = 'Login';
 	}
